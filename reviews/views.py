b0d870a4c18af2e69c89eb2ce133258fc82c1b71
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2020-12-13 13:11:04 -0700

    Refactored routes, added sort function to Keyboard and Mouse viewsets.

--- reviews/views.py
+++ reviews/views.py
@@ -11,18 +11,31 @@ class MouseViewset(viewsets.ModelViewSet):
     serializer_class = MouseSerializer
 
     @action(detail=False, methods=['GET'])
-    def by_brand(self, request):
-        queryset = Mouse.objects.filter(company__iexact=request.query_params.get('brand'))
-        serializer = MouseSerializer(queryset, many=True)
-        return Response(data=serializer.data)
+    def sort(self, request):
+        if 'brand' in request.query_params:
+            queryset = Mouse.objects.filter(company__iexact=request.query_params.get('brand'))
+            serializer = MouseSerializer(queryset, many=True)
+            return Response(data=serializer.data)
 
-    @action(detail=False, methods=['GET'])
-    def by_button_count(self, request):
-        queryset = Mouse.objects.filter(button_count=request.query_params.get('amount'))
-        serializer = MouseSerializer(queryset, many=True)
-        return Response(data=serializer.data)
+        if 'button_count' in request.query_params:
+            queryset = Mouse.objects.filter(button_count=request.query_params.get('amount'))
+            serializer = MouseSerializer(queryset, many=True)
+            return Response(data=serializer.data)
 
 
 class KeyboardViewset(viewsets.ModelViewSet):
     queryset = Keyboard.objects.all()
     serializer_class = KeyboardSerializer
+
+    @action(detail=False, methods=['GET'])
+    def sort(self, request):
+        if 'brand' in request.query_params:
+            queryset = Keyboard.objects.filter(company__iexact=request.query_params.get('brand'))
+            serializer = KeyboardSerializer(queryset, many=True)
+            return Response(data=serializer.data)
+
+        if 'backlit' in request.query_params:
+            queryset = Keyboard.objects.filter(is_backlit=request.query_params.get('backlit'))
+            serializer = KeyboardSerializer(queryset, many=True)
+            return Response(data=serializer.data)
+
